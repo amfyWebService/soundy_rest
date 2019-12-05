cc0c7404b062c511aaf65ad421b6d8633564a72f
Farsen976 <farsen.abdallahcombo@ynov.com>
2019-12-05 10:33:05 +0100

    refactor: resolve code review discussions

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,19 @@
 // config.ts
 const dotenv = require('dotenv-defaults');
+const urlJoin = require('url-join');
 dotenv.config();
-const getUserRootFolder =  () => {
-    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-  }
-export default {
+const getUserRootFolder = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+
+  export default {
     env: process.env.NODE_ENV,
     isDev: process.env.NODE_ENV === "development",
     isProd: process.env.NODE_ENV === "production",
     endpoint: process.env.API_URL,
     port: process.env.PORT,
     AMQP_HOST: process.env.AMQP_HOST,
-    HOME_UPLOAD_PATH: process.env.HOME_UPLOAD_PATH,
-    HOME_DIR: getUserRootFolder()
+
+    // PATH
+    PROJECT_DIR: __dirname,
+    HOME_DIR: getUserRootFolder,
+    HOME_UPLOAD_DIR: urlJoin(getUserRootFolder, process.env.HOME_UPLOAD_PATH),
 }
