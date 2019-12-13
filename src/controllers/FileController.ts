182849e0a627f445a626559a342c8215d87727b3
Goms <gomesdpro@gmail.com>
2019-12-13 12:10:28 +0100

    fix(track)

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,51 +1,51 @@
-import { JsonController, Post, UploadedFiles, BadRequestError, InternalServerError, Authorized, CurrentUser } from "routing-controllers"
+import { JsonController, Post, UploadedFiles, BadRequestError, InternalServerError, Authorized, CurrentUser, UploadedFile } from "routing-controllers"
 import config from '../config'
 import filesystem from 'fs'
 import uuid from "uuid"
 import path from "path";
-import urljoin from "url-join";
 import MqService, { QueueResponse } from '@/core/MqService';
+import { logger } from '@/shared';
+import BaseController from './BaseController';
 
 @JsonController("/")
-export class FileController {
-    private static DIR_MUSIC: string = urljoin(config.HOME_UPLOAD_DIR, "musics");
-    private static DIR_COVER: string = urljoin(config.HOME_UPLOAD_DIR, "covers");
+export class FileController extends BaseController {
+    private static DIR_MUSIC: string = path.join(config.HOME_UPLOAD_DIR, "musics");
+    private static DIR_COVER: string = path.join(config.HOME_UPLOAD_DIR, "covers");
 
-    @Post("upload")
-    async upload(@UploadedFiles('file', {required: true, options: {}}) files: Array<Express.Multer.File>, @CurrentUser() user: any) {
-        if (!files) {
-            throw new BadRequestError('No file was uploaded')
-        }
-
-        let filePath = "";
-        let promises: Promise<QueueResponse>[] = [];
-        for (let file of files ){
-            if (file.mimetype == "audio/mpeg") {
-                filePath = urljoin(FileController.DIR_MUSIC, user.id, uuid.v4() + ".mp3");
-            } else if(file.mimetype == "image/jpeg"){
-                filePath = urljoin(FileController.DIR_COVER, user.id, uuid.v4() + ".jpg");
-            } else {
-                throw new BadRequestError('File type not supported')
-            }
-
-            try {
-                await this.registerFile(filePath, file);
-
-                if (file.mimetype == "audio/mpeg") {
-                    promises.push(MqService.query("createTrack", {link: path.basename(filePath), user}));
-                }
-            } catch (e) {
-                throw new InternalServerError('An error occurred while registering the file')
-            }
-        }
-
-        return Promise.all(promises);
+    constructor() {
+        super();
+        logger.info("Upload directories", { music: FileController.DIR_MUSIC, cover: FileController.DIR_COVER }, null);
     }
 
-    private registerFile(filePath: string, file: Express.Multer.File) {
+    @Post("upload")
+    async upload(@UploadedFile('file', { required: true, options: {} }) file: Express.Multer.File, @CurrentUser({ required: true }) user: any) {
+        let filePath = "";
+
+        if (file.mimetype == "audio/mpeg") {
+            filePath = path.join(FileController.DIR_MUSIC, user._id, uuid.v4() + ".mp3");
+        } else if (file.mimetype == "image/jpeg") {
+            filePath = path.join(FileController.DIR_COVER, user._id, uuid.v4() + ".jpg");
+        } else {
+            throw new BadRequestError('File type not supported')
+        }
+
+        try {
+            await this.registerFile(filePath, file);
+
+            if (file.mimetype == "audio/mpeg") {
+                const res = await MqService.query("createTrack", { link: path.basename(filePath) }, user);
+                return this.handleResponse(res);
+            }
+        } catch (e) {
+            logger.error(e);
+            throw new InternalServerError('An error occurred while registering the file')
+        }
+    }
+
+    private registerFile(filePath: string, file: Express.Multer.File): Promise<void> {
         return new Promise((resolve, reject) => {
             filesystem.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
-                if (err){
+                if (err) {
                     reject(err);
                 } else {
                     filesystem.writeFileSync(filePath, Buffer.from(new Uint8Array(file.buffer)));
