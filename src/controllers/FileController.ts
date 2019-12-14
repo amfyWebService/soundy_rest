16bdfe0f0658f0bdb70679fac595fda2c3cf2ec3
Goms <gomesdpro@gmail.com>
2019-12-14 01:31:33 +0100

    feat: stream track file + refactor playlist

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,13 +1,14 @@
-import { JsonController, Post, UploadedFiles, BadRequestError, InternalServerError, Authorized, CurrentUser, UploadedFile } from "routing-controllers"
+import { JsonController, Post, BadRequestError, InternalServerError, CurrentUser, UploadedFile, Get, Param, Res, NotFoundError } from "routing-controllers"
 import config from '../config'
-import filesystem from 'fs'
 import uuid from "uuid"
 import path from "path";
-import MqService, { QueueResponse } from '@/core/MqService';
+import MqService from '@/core/MqService';
 import { logger } from '@/shared';
 import BaseController from './BaseController';
+import { Response } from 'express';
+import * as fs from "fs";
 
-@JsonController("/")
+@JsonController("")
 export class FileController extends BaseController {
     private static DIR_MUSIC: string = path.join(config.HOME_UPLOAD_DIR, "musics");
     private static DIR_COVER: string = path.join(config.HOME_UPLOAD_DIR, "covers");
@@ -17,7 +18,7 @@ export class FileController extends BaseController {
         logger.info("Upload directories", { music: FileController.DIR_MUSIC, cover: FileController.DIR_COVER }, null);
     }
 
-    @Post("upload")
+    @Post("/upload")
     async upload(@UploadedFile('file', { required: true, options: {} }) file: Express.Multer.File, @CurrentUser({ required: true }) user: any) {
         let filePath = "";
 
@@ -42,16 +43,40 @@ export class FileController extends BaseController {
         }
     }
 
+    @Get("/tracks/:id/stream")
+    public async trackStream(@Param("id") id: string, @Res() res: Response){
+        const track: any = this.handleResponse(await MqService.query("getTrack", {trackID: id}));
+
+        let filePath = path.join(track.owner, track.link);
+        // if(!fs.existsSync(filePath)){
+        //     throw new NotFoundError("Track file not found");
+        // }
+        
+        try {
+            await new Promise((resolve, reject) => {
+                res.sendFile(filePath, {root: FileController.DIR_MUSIC}, (err) => {
+                    if (err) reject(err);
+                    else resolve();
+                });
+            });
+        } catch (error) {
+            logger.error(error);
+        }
+        
+        return res;
+    }
+
     private registerFile(filePath: string, file: Express.Multer.File): Promise<void> {
         return new Promise((resolve, reject) => {
-            filesystem.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
+            fs.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
                 if (err) {
                     reject(err);
                 } else {
-                    filesystem.writeFileSync(filePath, Buffer.from(new Uint8Array(file.buffer)));
+                    fs.writeFileSync(filePath, Buffer.from(new Uint8Array(file.buffer)));
                     resolve();
                 }
             });
         });
     }
+    
 }
--- src/controllers/PlaylistController.ts
+++ src/controllers/PlaylistController.ts
@@ -1,5 +1,5 @@
 import BaseController from './BaseController';
-import { JsonController, Get, Param, CurrentUser, Post, Body } from 'routing-controllers';
+import { JsonController, Get, Param, CurrentUser, Post, Body, Put } from 'routing-controllers';
 import MqService from '@/core/MqService';
 import { IsString, MinLength } from 'class-validator';
 
@@ -12,13 +12,9 @@ export class addTrackToPlaylistBody {
     @IsString()
     @MinLength(10)
     trackID: string;
-
-    @IsString()
-    @MinLength(10)
-    playlistID: string;
 }
 
-@JsonController("/playlist")
+@JsonController("/playlists")
 export class PlaylistController extends BaseController {
 
     @Get("/:id")
@@ -31,27 +27,29 @@ export class PlaylistController extends BaseController {
 
     @Get("/user/:id")
     async getPlaylistsByUserID(@Param("id") id: string, @CurrentUser() user: any) {
-        const res = await MqService.query("getPlaylistsByUserID", { userID: id }, user);
+        let userID = id === "me" ? user._id : id;
+
+        const res = await MqService.query("getPlaylistsByUserID", { userID: userID }, user);
 
         return this.handleResponse(res, {
         });
     }
 
     @Post()
-    async createPlaylist(@Body({ required: true, validate: true }) body : createPlaylistBody, @CurrentUser() user: any) {
-        
-        const res = await MqService.query("createPlaylist", {name: body.name, user: user});
+    async createPlaylist(@Body({ required: true, validate: true }) body: createPlaylistBody, @CurrentUser() user: any) {
+
+        const res = await MqService.query("createPlaylist", { name: body.name, user: user });
 
         return this.handleResponse(res, {
         });
     }
 
-    @Post("/track")
-    async addTrackToPlaylist(@Body({ required: true, validate: true }) body : addTrackToPlaylistBody) {
-        const res = await MqService.query("addTrackToPlaylist", body);
+    @Put("/:playlist_id/track")
+    async addTrackToPlaylist(@Param("playlist_id") playlistId: string, @Body({ required: true, validate: true }) body: addTrackToPlaylistBody) {
+        const res = await MqService.query("addTrackToPlaylist", {trackID: body.trackID, playlistID: playlistId});
 
         return this.handleResponse(res, {
-            "music_already_in_playlist" : 400
+            "music_already_in_playlist": 400
         });
     }
 }
